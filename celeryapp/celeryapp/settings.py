ec95dc57a9071ba227338cc4b4d50bc69e1229f4
Naveenprakash <naveenprakashjk24@gmail.com>
2025-10-15 21:36:26 +0530

    Update settings.py

--- celeryapp/celeryapp/settings.py
+++ celeryapp/celeryapp/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-via=$x%+_!7jrnzpl!_st&q9@%iya+8!n$#9s-rf!$lnv0s4t@'
+# SECRET_KEY = 'django-insecure-via=$x%+_!7jrnzpl!_st&q9@%iya+8!n$#9s-rf!$lnv0s4t@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
